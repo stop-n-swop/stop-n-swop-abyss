72f147dbf1d66faf34e614f8e3a1d4e9cc94198a
Jack Ellis <jack.m.ellis@hotmail.co.uk>
2021-07-12 14:47:23 +0100

    feat: verify kyc docs

--- src/payments.ts
+++ src/payments.ts
@@ -4,6 +4,10 @@ export enum PaymentErrorCode {
   MISSING_REGISTER_FIELDS = "MISSING_REGISTER_FIELDS",
   FAILED_TO_REGISTER = "FAILED_TO_REGISTER",
   BANK_ACCOUNT_FAIL = "BANK_ACCOUNT_FAIL",
+  KYC_DOCUMENT_FAILED = "KYC_DOCUMENT_FAILED",
+  KYC_PAGE_TOO_SMALL = "KYC_PAGE_TOO_SMALL",
+  KYC_PAGE_FAILED = "KYC_PAGE_FAILED",
+  KYC_SUBMIT_FAILED = "KYC_SUBMIT_FAILED",
 }
 
 export class MissingRegisterFieldsError extends BadRequestError {
@@ -24,4 +28,24 @@ export class FailedToRegisterError extends UnknownError {
 
 export class BankAccountFailError extends UnknownError {
   code = PaymentErrorCode.BANK_ACCOUNT_FAIL;
+}
+
+export class KycDocumentFailedError extends UnknownError {
+  code = PaymentErrorCode.KYC_DOCUMENT_FAILED;
+}
+
+export class KycPageTooSmallError extends BadRequestError {
+  code = PaymentErrorCode.KYC_PAGE_TOO_SMALL;
+
+  toString() {
+    return "The uploaded file is too small";
+  }
+}
+
+export class KycPageFailedError extends UnknownError {
+  code = PaymentErrorCode.KYC_PAGE_FAILED;
+}
+
+export class KycSubmitFailedError extends UnknownError {
+  code = PaymentErrorCode.KYC_SUBMIT_FAILED;
 }
