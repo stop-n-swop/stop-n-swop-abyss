287d330f5ca9accc35e71bd60099edbe66e7257c
Jack Ellis <jack.m.ellis@hotmail.co.uk>
2021-11-12 20:14:52 +0000

    feat: email not unique error

--- src/user.ts
+++ src/user.ts
@@ -2,6 +2,7 @@ import { ConflictError, NotFoundError } from "./common";
 
 export enum UserErrorCode {
   USERNAME_NOT_UNIQUE = "USERNAME_NOT_UNIQUE",
+  EMAIL_NOT_UNIQUE = "EMAIL_NOT_UNIQUE",
   USER_NOT_FOUND = "USER_NOT_FOUND",
 }
 
@@ -10,6 +11,30 @@ export class UsernameNotUniqueError extends ConflictError {
 
   toString() {
     return "This username has already been picked by another user";
+  }
+}
+
+export class EmailNotUniqueError extends ConflictError {
+  code = UserErrorCode.EMAIL_NOT_UNIQUE
+
+  constructor(public email: string) {
+    super();
+  }
+
+  toString() {
+    return `The email associated with this account (${this.email}) has already been registered. Please log in with your original account, you can then add other login methods from the settings page.`
+  }
+  
+  toHttpResponse() {
+    const response = super.toHttpResponse()
+
+    return {
+      ...response,
+      body: {
+        ...response.body,
+        email: this.email,
+      }
+    }
   }
 }
 
