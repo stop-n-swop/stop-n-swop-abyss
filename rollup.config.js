512d4530217eaca3c208807f1e6bd7a50b1aa44d
Jack Ellis <jack.m.ellis@hotmail.co.uk>
2022-12-06 12:27:06 +0000

    fix: ulid should be an external dependency

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,33 @@
-import babel from "rollup-plugin-babel";
-import localResolve from "rollup-plugin-node-resolve";
-import cleanup from "rollup-plugin-cleanup";
+import babel from 'rollup-plugin-babel';
+import localResolve from 'rollup-plugin-node-resolve';
+import cleanup from 'rollup-plugin-cleanup';
 
 const config = {
   input: `src/index.ts`,
   output: [
     {
-      file: "dist/es/abyss.js",
-      format: "es",
+      file: 'dist/es/abyss.js',
+      format: 'es',
     },
     {
       file: `dist/cjs/abyss.js`,
-      format: "cjs",
+      format: 'cjs',
     },
   ],
   plugins: [
     localResolve({
-      extensions: [".js", ".ts"],
+      extensions: ['.js', '.ts'],
     }),
     babel({
-      exclude: "node_modules/**",
-      extensions: [".js", ".ts"],
+      exclude: 'node_modules/**',
+      extensions: ['.js', '.ts'],
     }),
     cleanup({
-      extensions: ["js", "ts"],
+      extensions: ['js', 'ts'],
       sourcemap: false,
     }),
   ],
+  external: ['ulid'],
 };
 
 export default config;
